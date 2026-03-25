3c9e7719f4e855db9378606ce3ab73a05b848643
KusumKumari28 <aayushmanagarkoti5@gmail.com>
2026-03-25 16:37:59 -0400

    FPG1-71 implement role authorization middleware

--- src/routes/testRoutes.ts
+++ src/routes/testRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authMiddleware, { AuthRequest } from "../middleware/authMiddleware";
+import roleMiddleware from "../middleware/roleMiddleware";
 
 const router = express.Router();
 
@@ -15,5 +16,17 @@ router.get("/protected", authMiddleware, (req: AuthRequest, res) => {
     user: req.user,
   });
 });
+
+router.get(
+  "/admin",
+  authMiddleware,
+  roleMiddleware("admin"),
+  (req: AuthRequest, res) => {
+    res.json({
+      message: "Welcome admin",
+      user: req.user,
+    });
+  },
+);
 
 export default router;
