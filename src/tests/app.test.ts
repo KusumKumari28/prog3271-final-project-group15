c49a9ba94445a3d3f4479d3bf82557787b59f705
mohit sharma <Msharma3475@conestogac.on.ca>
2026-04-08 21:23:43 -0700

    FPG1-170 write unit tests

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,10 +1,9 @@
-   import request from "supertest";
-   import app from "../app";
+import request from "supertest";
+import app from "../app";
 
-   describe("App Test", () => {
-     it("should return Forum Backend Running on home route", async () => {
-       const response = await request(app).get("/");
-       expect(response.status).toBe(200);
-       expect(response.text).toBe("Forum Backend Running");
-     });
-   });
+describe("App Test", () => {
+  it("should return 200 for root route", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+  });
+});
