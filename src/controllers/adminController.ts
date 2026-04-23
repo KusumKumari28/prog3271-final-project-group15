7dda259920775eb96b3127425d6440ef015fc496
Manav1101 <mpatel11013@gmail.com>
2026-04-23 01:33:23 -0400

    feat: add getAllUsers endpoint for admin analytics

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -4,6 +4,7 @@ import Post from "../models/Post";
 import Comment from "../models/Comment";
 import Like from "../models/Like";
 
+// GET ANALYTICS - overall site statistics
 export const getAnalytics = async (req: Request, res: Response) => {
   try {
     const totalUsers = await User.countDocuments();
@@ -22,6 +23,20 @@ export const getAnalytics = async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(500).json({
       message: "Error fetching analytics",
+      error,
+    });
+  }
+};
+
+// GET ALL USERS - user analytics
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find().select("-password");
+
+    return res.status(200).json({ users });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching users",
       error,
     });
   }
